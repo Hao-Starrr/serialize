dad00847c1a73ad76dec0e7694471f07b4427712
Wang Wenhao <63582163+Hao-Starrr@users.noreply.github.com>
2023-10-24 16:33:42 -0400

    comment

--- src/serialize.h
+++ src/serialize.h
@@ -13,6 +13,16 @@
 // define normal template function
 ////////////////////////////////////////////////////
 
+/* 
+这里在定义一个通用的模板函数。
+用这个模板就可以用 serialize(xxx)去处理任何类，只要这个类里面定义了序列化的方法。
+serialize(xxx)和序列化基本的数据类型的写法是相同的，
+这样我要序列化一个东西的时候就不用管xxx到底是什么。
+
+通用的情况是直接调用它的成员函数,
+特殊的情况才使用int double 里面的写法。
+*/
+
 template<typename SerializableType>
 std::string serialize ( SerializableType& a )
 {
@@ -30,6 +40,22 @@ int deserialize ( std::string &str, SerializableType& a )
 //Serialize for C/C++ basic type
 //examples: short,int,float,long long,double
 /////////////////////////////////////////////////
+
+/*
+
+这里的写法非常秀操作，它全部是用宏来实现的。
+在预处理阶段，这些宏就会被全部替换为给每一个类型对应的serialize函数。
+如果你用重载，你就需要给每一种类型写一个函数。
+但是你用宏，当再增加一种数据类型的时候，你就只需要在后面加一行。
+这样他就不用给每一个类型写很多长得差不多的函数了。
+
+但C++中并不推荐这种写法。更推荐的写法是用模板元编程。
+他其实已经用了模板元编程，只不过他需要同时处理特殊的类型和自定义类型，他这里就只能用宏了。
+
+并且它这里硬是不指定命名空间，每一次使用的时候都要用global的命名空间。
+
+*/
+
 #define DEF_BASIC_TYPE_SERIALIZE(Type) \
  template<> \
 std::string inline serialize(Type& b) \
@@ -100,6 +126,11 @@ public:
     OutStream ( ) : os ( std::ios::binary )
     {
     }
+    /*
+    std::ostringstream os;是成员变量。
+    在这里新建了一个成员变量，每次创建auto stream的时候就会自动新建这个成员变量，
+    并指定了它的模式为二进制模式 。但这里二进制模式是没用的。
+    */
 
     template<typename SerializableType>
     OutStream& operator<< ( SerializableType& a )
@@ -132,6 +163,40 @@ public:
         std::copy ( a.begin ( ), a.end ( ), std::back_inserter ( temp ) );
         return this->operator<< ( temp );
     }
+    /*
+
+    我写我就会这样写:
+    template<typename T>
+    OutStream& operator<<(T& input)
+    {
+        std::string str = ::serialize(input);
+        os.write(str.data(), str.size());
+        return *this; // 我在class里面overload << 的时候 就不需要传入这个class了。
+    }
+    template<typename T>
+    OutStream& operator<<(std::vector<T>& input)
+    {
+        int len = input.size()
+        std::string str = ::serialize(len);
+        os.write(str.data(), str.size());
+
+        for(auto vec : input){ // 这里用到了C++新的特性auto和：
+            std::string str = ::serialize(vec);
+            os.write(str.data(), str.size());
+        }
+        return *this;
+    }
+    template<typename T>
+    OutStream& operator<<(std::list<T>& inputList)
+    {
+        std::vector<T> myVector(inputList.begin(), inputList.end());
+        // 这里是用一个list直接初始化一个vector。更简洁。
+        return this->operator<< ( myVector );
+    }
+
+    */
+
+    
 
     template<typename BasicType>
     OutStream& operator<< ( std::set<BasicType>& a )
@@ -263,7 +328,7 @@ protected:
 ////////////////////////////////////////////
 //Serialize for custom class object
 //If your class object want to be serialized,
-//Plese derive for this base class
+//Please derive for this base class
 ///////////////////////////////////////////
 
 class Serializable
